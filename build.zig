21848c4f3010449562f098e625e250184a42093e
Blustak <menditta1@gmail.com>
2025-05-16 14:12:14 +0100

    reset working tree

--- build.zig
+++ build.zig
@@ -1,47 +1,44 @@
 const std = @import("std");
-const rlz = @import("raylib_zig");
+
+//Testing bits taken from the zig docs.
+const test_targets = [_]std.Target.Query{
+    .{}, //native
+    .{
+        .cpu_arch = .x86_64,
+        .os_tag = .linux,
+    },
+    .{
+        .cpu_arch = .aarch64,
+        .os_tag = .macos,
+    },
+};
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    
-    const raylib_dep = b.dependency("raylib_zig", .{
+
+    const exe = b.addExecutable(.{
+        .name = "chip8-emulator",
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const raylib = raylib_dep.module("raylib");
-    const raylib_artifact = raylib_dep.artifact("raylib");
-
-    //web exports are completely separate
-    if (target.query.os_tag == .emscripten) {
-        const exe_lib = try rlz.emcc.compileForEmscripten(b, "chip8-emulator", "src/main.zig", target, optimize);
-
-        exe_lib.linkLibrary(raylib_artifact);
-        exe_lib.root_module.addImport("raylib", raylib);
-
-        // Note that raylib itself is not actually added to the exe_lib output file, so it also needs to be linked with emscripten.
-        const link_step = try rlz.emcc.linkWithEmscripten(b, &[_]*std.Build.Step.Compile{ exe_lib, raylib_artifact });
-        //this lets your program access files like "resources/my-image.png":
-        link_step.addArg("--embed-file");
-        link_step.addArg("resources/");
-
-        b.getInstallStep().dependOn(&link_step.step);
-        const run_step = try rlz.emcc.emscriptenRunStep(b);
-        run_step.step.dependOn(&link_step.step);
-        const run_option = b.step("run", "Run chip8-emulator");
-        run_option.dependOn(&run_step.step);
-        return;
-    }
-
-    const exe = b.addExecutable(.{ .name = "chip8-emulator", .root_source_file = b.path("src/main.zig"), .optimize = optimize, .target = target });
-
-    exe.linkLibrary(raylib_artifact);
-    exe.root_module.addImport("raylib", raylib);
-
-    const run_cmd = b.addRunArtifact(exe);
-    const run_step = b.step("run", "Run chip8-emulator");
-    run_step.dependOn(&run_cmd.step);
-
     b.installArtifact(exe);
+
+    const run_exe = b.addRunArtifact(exe);
+
+    const run_step = b.step("run", "Run the executable");
+    run_step.dependOn(&run_exe.step);
+
+    const test_step = b.step("test", "Run unit tests");
+    for (test_targets) |test_target| {
+        const unit_tests = b.addTest(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = b.resolveTargetQuery(test_target),
+        });
+        const run_unit_tests = b.addRunArtifact(unit_tests);
+        run_unit_tests.skip_foreign_checks = true;
+        test_step.dependOn(&run_unit_tests.step);
+    }
 }
