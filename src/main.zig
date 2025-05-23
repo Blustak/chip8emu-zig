de125c264641f0e2c728ab9a1897146c5aabafac
Blustak <menditta1@gmail.com>
2025-05-23 13:53:33 +0100

    applied renderer rewrite

--- src/main.zig
+++ src/main.zig
@@ -1,2 +1,7 @@
+const renderer = @import("renderer.zig");
+
 pub fn main() !void {
+    renderer.init();
+    defer renderer.deinit();
+    renderer.draw();
 }
--- src/renderer.zig
+++ src/renderer.zig
@@ -1,152 +1,71 @@
-//! This module handles drawing to the screen
-const config = @import("config");
-const std = @import("std");
-const assert = std.debug.assert;
 const rl = @cImport({
     @cInclude("raylib.h");
 });
+const std = @import("std");
+const assert = std.debug.assert;
 
-// const vbuf = [_]u64{0} ** 32;
-const PIXEL_SCALE = config.pixel_scale;
-const BG_COLOR = rl.DARKGRAY;
-const FG_COLOR = rl.GREEN;
-const WIDTH = 64;
-const HEIGHT = 32;
+const PIXEL_WIDTH = 64;
+const PIXEL_HEIGHT = 32;
+const PIXEL_SCALE = 20;
+const SCREEN_WIDTH = PIXEL_WIDTH * PIXEL_SCALE;
+const SCREEN_HEIGHT = PIXEL_HEIGHT * PIXEL_SCALE;
 
-const WIN_WIDTH = WIDTH * PIXEL_SCALE;
-const WIN_HEIGHT = HEIGHT * PIXEL_SCALE;
+const BACKGROUND_COLOUR = rl.BLACK;
+const FOREGROUND_COLOR = rl.GREEN;
 
-/// A sprite is a group of bytes which are a binary representation of the
-/// desired picture. Sprites may be up to 15 bytes.
+var vram: [PIXEL_HEIGHT][PIXEL_WIDTH]bool = .{.{false} ** PIXEL_WIDTH} ** PIXEL_HEIGHT;
 pub const Sprite = struct {
-    mem: []const [8]bool,
-    /// assertion optimizations and checking
-    pub fn new(mem: []const [8]bool) Sprite {
-        assert(mem.len < 16);
-        return Sprite{ .mem = mem };
-    }
+    data: []const [8]bool,
 };
-/// Video buffer - what the renderer reads to draw to the screen.
-pub const Vbuf = struct {
-    mem: [][]bool,
-    len: usize,
-    pub fn new(buf: [][]bool) Vbuf {
-        assert(buf.len == HEIGHT);
-        return Vbuf{ .mem = buf, .len = buf.len };
-    }
-    pub fn clear(self: *Vbuf) void {
-        for (self.mem) |*row| {
-            @memset(row.*, false);
-        }
-    }
-};
-
-pub fn openWindow(vram: *Vbuf) void {
-    init();
-    defer deinit();
-    while (!rl.WindowShouldClose()) {
-        draw(vram);
+// Convert a u8 to an array of bools.
+pub fn u8_to_data(data: u8) [8]bool {
+    var out: [8]bool = .{false} ** 8;
+    inline for (0..8) |i| {
+        out[i] = (((data >> i) % 2) == 0);
     }
 }
 
-fn draw(vram: *Vbuf) void {
-    rl.BeginDrawing();
-    defer rl.EndDrawing();
-    rl.ClearBackground(BG_COLOR);
-    for (vram.mem, 0..) |row, i| {
-        for (row, 0..) |px, j| {
-            if (px) {
+pub fn init() void {
+    rl.InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Chip 8 emulator");
+    rl.SetTargetFPS(60);
+}
+
+pub fn deinit() void {
+    rl.CloseWindow();
+}
+
+pub fn draw() void {
+    while (!rl.WindowShouldClose()) {
+        rl.BeginDrawing();
+        defer rl.EndDrawing();
+
+        rl.ClearBackground(BACKGROUND_COLOUR);
+
+        @setEvalBranchQuota((SCREEN_HEIGHT * SCREEN_WIDTH) + 1);
+        inline for (vram, 0..) |row, y| {
+            inline for (row, 0..) |px, x| {
                 rl.DrawRectangle(
-                    PIXEL_SCALE * j,
-                    PIXEL_SCALE * i,
-                    PIXEL_SCALE,
-                    PIXEL_SCALE,
-                    FG_COLOR,
+                    x * PIXEL_SCALE,
+                    y * PIXEL_SCALE,
+                    (x + 1) * PIXEL_SCALE,
+                    (y + 1) * PIXEL_SCALE,
+                    if (px) FOREGROUND_COLOR else BACKGROUND_COLOUR,
                 );
             }
         }
     }
 }
 
-fn init() void {
-    rl.InitWindow(WIN_WIDTH, WIN_HEIGHT, "Chip-8 emu-rl");
-    rl.SetTargetFPS(60);
-}
-
-fn deinit() void {
-    rl.CloseWindow();
-}
-
-pub fn draw_sprite(vbuf: *Vbuf, sprite: *const Sprite, x: usize, y: usize) void {
-    assert(x < WIDTH);
-    assert(y < HEIGHT);
-    const sprite_width: usize = if (WIDTH - x < 8) WIDTH - x else 8;
-    const sprite_height: usize = if (HEIGHT - y < sprite.mem.len) HEIGHT - y else sprite.mem.len;
-}
-
-test "clear test" {
-    const expect = std.testing.expect;
-    var buf: [HEIGHT][]bool = undefined;
-    inline for (0..HEIGHT) |i| {
-        var row = [_]bool{false} ** WIDTH;
-        buf[i] = &row;
-    }
-    var vbuf = Vbuf.new(&buf);
-    vbuf.clear();
-    for (vbuf.mem) |row| {
-        for (row) |b| {
-            try expect(!b);
+pub fn write_sprite(sprite: *const Sprite, x: usize, y: usize) void {
+    assert(x < PIXEL_WIDTH);
+    assert(y < PIXEL_HEIGHT);
+    const sprite_width = if (PIXEL_WIDTH - x > 8) 8 else PIXEL_WIDTH - x;
+    const sprite_height = if (PIXEL_HEIGHT - y > sprite.data.len) sprite.data.len else PIXEL_HEIGHT - y;
+    for (0..sprite_height) |i| {
+        const vram_slice = vram[y + i][x..(x + sprite_width)];
+        for (vram_slice, 0..) |*b, j| {
+            const sprite_px = sprite.*.data[i][j];
+            b.* = if (b.* and sprite_px) false else b.* or sprite_px;
         }
-    }
-    for (vbuf.mem) |*row| {
-        @memset(row.*, true);
-    }
-    vbuf.clear();
-    for (vbuf.mem) |row| {
-        for (row) |b| {
-            try expect(!b);
-        }
-    }
-    for (buf) |row| {
-        for (row) |b| {
-            try expect(!b);
-        }
-    }
-}
-
-test "draw_sprite test" {
-    const expectEqualSlices = std.testing.expectEqualSlices;
-    var buf: [HEIGHT][]bool = undefined;
-    inline for (0..HEIGHT) |i| {
-        var row = [_]bool{false} ** WIDTH;
-        buf[i] = &row;
-    }
-    var vbuf = Vbuf.new(&buf);
-    const sprite_empty_data = [_]bool{false} ** 8;
-    const tiny_sprite_data = [2][8]bool{
-        .{ true, true, true, true, false, false, false, false },
-        .{false} ** 8,
-    };
-    const big_sprite_data = [_][8]bool{[_]bool{true} ** 8} ** 4;
-    const empty_sprite = Sprite.new(&.{sprite_empty_data});
-    const tiny_sprite = Sprite.new(&tiny_sprite_data);
-    const big_sprite = Sprite.new(&big_sprite_data);
-
-    draw_sprite(&vbuf, &empty_sprite, 5, 5);
-    try expectEqualSlices([]bool, &tiny_sprite_data, vbuf.mem[5][5..13]);
-    vbuf.clear();
-
-    draw_sprite(&vbuf, &tiny_sprite, 1, 0);
-    inline for (tiny_sprite_data, 0..) |sprite_row, i| {
-        try expectEqualSlices([]bool, &sprite_row, vbuf.mem[i][1..9]);
-    }
-    draw_sprite(&vbuf, &tiny_sprite, 1, 0);
-    inline for (tiny_sprite_data, 0..) |sprite_row, i| {
-        try expectEqualSlices([]bool, &sprite_row, vbuf.mem[i][1..9]);
-    }
-    vbuf.clear();
-    draw_sprite(&vbuf, &big_sprite, 0, 0);
-    inline for (big_sprite_data, 0..) |sprite_row, i| {
-        try expectEqualSlices([]bool, &sprite_row, vbuf.mem[i][0..8]);
     }
 }
